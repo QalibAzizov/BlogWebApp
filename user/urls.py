d06337486e6515386a2306ccf8b9a7244a434406
Your Name <azizovgalib@gmail.com>
2022-12-01 19:17:02 +0400

    login_logout

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from user.views import register
+from django.contrib.auth import views as auth_views
+from user.views import register, profile
 
 
 urlpatterns = [
     
     path('register/', register, name = 'register'),
+     path('profile/', profile, name = 'profile'),
+    path('login/', auth_views.LoginView.as_view(template_name= 'login.html'), name = 'login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name= 'logout.html'), name = 'logout'),
       
 ]
