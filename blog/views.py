1cead2edfd983f11bf1d44b48993c860daee6824
Your Name <azizovgalib@gmail.com>
2022-12-05 11:55:07 +0400

    post_update_delete

--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,72 @@
 from django.shortcuts import render
-from django.http import  HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    ListView, 
+    DetailView, 
+    CreateView,
+    UpdateView,
+    DeleteView
+    )
+
 from blog.models import Post
-# Create your views here.
 
 
+class PostListView(ListView):
+    model = Post
+    template_name = 'home.html' 
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
 
 
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
 
-def home(request):
-    posts = Post.objects.all()
 
-    context = {
-        'posts': posts
-    }
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post     
+    template_name = 'post_form.html'
+    fields = ['title','content']
+   
 
-    return render(request, 'home.html', context)
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post     
+    template_name = 'post_form.html'
+    fields = ['title','content']
+   
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'post_confirm_delete.html'
+    success_url = '/blog'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 
 def about(request):
     return render(request, 'about.html', {'title': 'About'})
+
+
+
     
