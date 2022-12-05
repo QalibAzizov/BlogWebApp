7abf0a642f072d58e0d19665f9aebefecbc1c850
Your Name <azizovgalib@gmail.com>
2022-12-05 15:46:13 +0400

    paginations

--- blog/urls.py
+++ blog/urls.py
@@ -6,11 +6,13 @@ from blog.views import(
     PostCreateView,
     PostUpdateView,
     PostDeleteView,
+    UserPostListView,
     )
 
 
 urlpatterns = [
     path('', PostListView.as_view(), name='home'),
+    path('user/<str:username>/', UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
